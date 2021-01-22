78f4bf6c54cf5e13e3b01e9e36e036f4c4e91dff
SAMIA <68030713+lorjoh@users.noreply.github.com>
2021-01-22 13:03:33 -0500

    heroku time

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
-var dbJson = require("./db/db.json");
+const dbJson = require("./db/db.json");
+
+let dbFile = path.join(__dirname, './db/db.json');
 
 var app = express();
 var port = process.env.PORT || 8000;
 
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json);
+app.use(express.json());
+// app.use(express.static('public'));
 app.use(express.static(__dirname, 'public'));
 
+// html routes
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-
 });
 
 app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-
 });
 
+// api routes
 app.get('/api/notes', function (req, res) {
     fs.readFile('./db/db.json', 'utf8', function (err, data) {
         if (err) {
@@ -35,7 +38,7 @@ app.post('/api/notes', function (req, res) {
 
     function genNewPost(newPost) {
         let id = 1;
-        for (let i = o; i < dbJson.length; i++) {
+        for (let i = 0; i < dbJson.length; i++) {
             let Post = dbJson[i];
             if (Post.id > id) {
                 id = Post.id
@@ -46,7 +49,7 @@ app.post('/api/notes', function (req, res) {
         dbJson.push(newPost);
         return JSON.stringify(dbJson);
     }
-    fs.writeFile('./db/db.json', genNewPost(newPost), function (err) {
+    fs.writeFile(dbFile, genNewPost(newPost), function (err) {
         if (err) {
             throw err;
         }
@@ -55,6 +58,32 @@ app.post('/api/notes', function (req, res) {
     res.json(newPost);
 });
 
+
+app.delete('/api/notes/:id', function (req, res) {
+
+    // function deletePost(dbJson) {
+
+    for (let i = 0; i < dbJson.length; i++) {
+        if (dbJson[i].id == req.params.id) {
+            dbJson.splice(i, 1);
+            break;
+        }
+    }
+    //  return JSON.stringify(dbJson)
+    // }
+
+    fs.writeFile(dbFile, JSON.stringify(dbJson), function (err) {
+        // fs.writeFile(dbFile, deletePost(dbJson), function (err) {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("Post deleted");
+        }
+    });
+    res.json(db);
+});
+
+
 app.listen(port, function () {
-    console.log(" message received from PORT" + PORT)
+    console.log(" message received from PORT" + port)
 });
